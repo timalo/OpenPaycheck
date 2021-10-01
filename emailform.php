67354b4afe5fb0d8e98d99bb070a70604783f6ee
VilleLahdenpera <ville.lahdenp@hotmail.fi>
2021-10-01 13:02:45 +0300

    Add echo print

--- emailform.php
+++ emailform.php
@@ -5,7 +5,7 @@ if($_POST["message"]) {
 mail("ville.lahdenp@hotmail.fi", "OpenPayCheck Invitation",
 
 $_POST["Tällä linkillä pääset vertailemaan palkkatietoja ryhmässä:"]. "From: ville.lahdenp@hotmail.fi");
-
+echo "paksa";
 }
 
 ?>

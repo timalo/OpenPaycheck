bd531de0aa1c23b83b73f11a8f657240b80a8ec6
VilleLahdenpera <ville.lahdenp@hotmail.fi>
2021-10-05 14:07:13 +0300

    Improvement on emailform

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-document.write("TEST!");
-alert("Alert!")
+
+
+
+
+function add_inputs() {
+    n = $('#my_input').val();
+    $("#rolonum").html('');
+    for (var i = 1; i <= n; i++) {
+      $("#rolonum").append('<span>Member ' + i + ' </span><input id="rolo_add' + i + '" name="addposition"  type="email" value="" required/><br>');
+
+    }
+
+  }
+  

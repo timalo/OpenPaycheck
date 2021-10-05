b534ae3697a285a1312f33ce1fc8a2ab0a7cf262
VilleLahdenpera <ville.lahdenp@hotmail.fi>
2021-10-05 16:21:42 +0300

    Changes to emailform and main.js

--- main.js
+++ main.js
@@ -9,6 +9,19 @@ function add_inputs() {
       $("#rolonum").append('<span>Member ' + i + ' </span><input id="rolo_add' + i + '" name="addposition"  type="email" value="" required/><br>');
 
     }
-
   }
+
+    function returnText(){
+      let groupName = document.getElementById("groupName").value;
+      let email_1 = document.getElementById("email_1").value;
+      n = $('#my_input').val();
+      var arr = [];
+
+      for (var i = 1; i <= n; i++) {
+        arr.push(document.getElementById("rolo_add" + i).value);
+    }
+      alert(arr);
+  }
+
+
   

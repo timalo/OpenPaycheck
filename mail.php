ca58c648c9388f8ca7b57a84eb75f001733dc893
jannunen18 <44237491+jannunen18@users.noreply.github.com>
2021-10-12 15:28:08 +0300

    Get array values to Mail.php (#6)

--- mail.php
+++ mail.php
@@ -1,16 +1,19 @@
 <?php
 
-
-  
-
+$perse = $_POST['email'];
+echo "JEEE ";
+echo $perse[2];
+echo implode(", ", $perse);
 $from = "tonykfc@lkybast.com";
-$to = "ville.lahdenp@hotmail.fi";
+$to = "limpeh@lkybast.com";
 $subject = "PHP Mail Test script";
 $message = "This is a test to check the PHP Mail functionality";
 $headers = "From:" . $from;
-mail($to,$subject,$message, $headers);
+
+for ($i = 0; $i < count($perse); $i++) {
+    mail($perse[$i],$subject,$message, $headers);
+}
+
 echo "Test email sent";
-
-
 
 ?>

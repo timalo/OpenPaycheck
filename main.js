2c4a6c658ce3b19c2a959d0b4b6c8f9599078f90
Juho Annunen <jannunen18@student.oulu.fi>
2021-10-12 15:26:07 +0300

    Get array values to Mail.php

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 function add_inputs() {
   n = $('#my_input').val();
-  $("#rolonum").html('');
+  $("#emailform").html('');
   for (var i = 1; i <= n; i++) {
-    $("#rolonum").append('<span>Member ' + i + ' </span><input id="rolo_add' + i + '" name="addposition"  type="email" value="" required/><br>');
+    $("#emailform").append('<span>Member ' + i + ' </span><input id="rolo_add' + i + '" name="email[]" type="email" value="" required/><br>');
 
     }
+    $("#emailform").append('<input type="submit" value="Submit">');
   }
 
 function returnText(){
+  console.log("paska");
   let groupName = document.getElementById("groupName").value;
   let email_1 = document.getElementById("email_1").value;
   n = $('#my_input').val();
@@ -17,7 +19,7 @@ function returnText(){
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open("GET", "mail.php");
   xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xmlhttp.send();
+  xmlhttp.send(email_1);
   xmlhttp.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
       display.innerHTML = this.responseText;
@@ -25,6 +27,8 @@ function returnText(){
       display.innerHTML = "Loading...";
     };
   }
+  
+
 
 
   for (var i = 1; i <= n; i++) {

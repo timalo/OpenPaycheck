92411caa71e39776cf408ceca5244e2ecf1276e9
jannunen18 <44237491+jannunen18@users.noreply.github.com>
2021-10-22 10:23:40 +0300

    improvements to results, emailform and mail (#11)
    Co-authored-by: VilleLahdenpera <ville.lahdenp@hotmail.fi>

--- mail.php
+++ mail.php
@@ -1,15 +1,9 @@
 <?php
 
 $to = $_POST['email'];
-echo "JEEE ";
-
-echo implode(", ", $to);
-
-$from = "tonykfc@lkybast.com";
-
-$subject = "PHP Mail Test script";
+$from = "openpaycheck.send@gmail.com";
+$subject = "OpenPayCheck Invitation";
 $group = $_POST['group_name'];
-echo $group;
 
 $headers = "From:" . $from;
 
@@ -28,7 +22,7 @@ if($conn->connect_error) {
 for ($i = 0; $i < count($to); $i++) {
     $key = md5((uniqid()));
 
-    $message = "You have been invited to compare salary information in a group: $group  Follow this link to give your information:  http://localhost/OpenPaycheck/salary.php?num=$key";
+    $message = "Hi! \n\nYou have been invited to compare salary information in a group: $group  \nFollow this link to give your information:\n\nhttp://localhost/OpenPaycheck/salary.php?num=$key  \n\nBest Regards,\nOpenPayCheck";
 
     //save linkKey to db
     $saveKeyQuery = "INSERT INTO users (userEmail, linkKey, userGroup) VALUES ('$to[$i]', '$key', '$group')";
@@ -38,7 +32,6 @@ for ($i = 0; $i < count($to); $i++) {
 
 $conn->close();
 
-echo "Test email sent";
 
 header("Location: http://localhost/OpenPaycheck/emailConfirmation.html");
 
--- main.js
+++ main.js
@@ -1,46 +1,15 @@
 function add_inputs() {
   n = $('#my_input').val();
   $("#emailform").html('');
-  $("#emailform").append('<label for="group_name">Group name:</label>');
-  $("#emailform").append('<input type="text" name="group_name" value="" ><br>');
-  for (var i = 1; i <= n; i++) {
-    $("#emailform").append('<span>Member ' + i + ' </span><input id="rolo_add' + i + '" name="email[]" type="email" value="" required/><br>');
+  $("#emailform").append('<br><label for="group_name">Group name:</label>');
+  $("#emailform").append('<input type="text" name="group_name" value="" ><br><br>');
+  $("#emailform").append('<label for="your_email">Your email:</label>');
+  $("#emailform").append('<input id="rolo_add' + 0 + '" name="email[]" type="email" value="" required/><br>');
+  for (var i = 2; i <= n; i++) {
+    $("#emailform").append('<span>Member ' + i + ': </span><input id="rolo_add' + i + '" name="email[]" type="email" value="" required/><br>');
     
 
     }
-    $("#emailform").append('<input type="submit" value="Submit">');
+    $("#emailform").append('<br><input type="submit" value="Submit">');
   }
 
-
-
-  
-
-
-/*
-function returnText(){
-  console.log("paska");
-  let groupName = document.getElementById("groupName").value;
-  let email_1 = document.getElementById("email_1").value;
-  n = $('#my_input').val();
-  var arr = [];
-
-  var display = document.getElementById("content");
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", "mail.php");
-  xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xmlhttp.send(email_1);
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      display.innerHTML = this.responseText;
-    } else {
-      display.innerHTML = "Loading...";
-    };
-  }
-  
-
-  for (var i = 1; i <= n; i++) {
-    arr.push(document.getElementById("rolo_add" + i).value);
-}
-  alert(arr);
-}  
-*/
